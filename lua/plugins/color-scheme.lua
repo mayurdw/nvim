f937f689c77f6f9e6e08c16a7e72a6fa28308ae3
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-09-29 15:43:15 +1300

    Create color-scheme.lua

---
+++ lua/plugins/color-scheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- add gruvbox
+  { "EdenEast/nightfox.nvim" }, -- lazy
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "carbonfox",
+    },
+  },
+}
