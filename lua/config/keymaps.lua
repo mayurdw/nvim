70b1608227fae52f945ad00a3ed97743c28f0c54
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-01-06 18:23:30 +1300

    Restarting config

--- lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set("n", "<leader>y", function()
-  vim.fn.setreg(vim.v.register, vim.fn.expand("%"))
-end, { desc = "Copy filename+line to clipboard" })
-vim.keymap.set("i", "jk", "<Esc><cmd>w<CR>", { desc = "Stop typing and save file" })
