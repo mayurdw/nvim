73b7c8db09b9fce2b7c50f8862521e99831ced36
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-02-01 12:04:01 +1300

    Removed Unnecesary config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,17 +1,13 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            vim.lsp.enable('lua_ls')
-        end,
-        keys = {
-            { "K",          vim.lsp.buf.hover,          desc = "Hover" },
-            { "<leader>rn", vim.lsp.buf.rename,         desc = "Rename item" },
-            { "<leader>rf", vim.lsp.buf.format,         desc = "Perform Formatting" },
-            { "gd",         vim.lsp.buf.declaration,    desc = "Go to Declaration" },
-            { "gi",         vim.lsp.buf.implementation, desc = "Go to Implementation" },
-            { "gr",         vim.lsp.buf.references,     desc = "Go to references" },
-            { "<leader>am", vim.lsp.buf.code_action,    desc = "Code Action" },
-        }
+    "neovim/nvim-lspconfig",
+    keys = {
+        { "K",          vim.lsp.buf.hover,          desc = "Hover" },
+        { "<leader>rn", vim.lsp.buf.rename,         desc = "Rename item" },
+        { "<leader>rf", vim.lsp.buf.format,         desc = "Perform Formatting" },
+        { "gd",         vim.lsp.buf.declaration,    desc = "Go to Declaration" },
+        { "gi",         vim.lsp.buf.implementation, desc = "Go to Implementation" },
+        { "gr",         vim.lsp.buf.references,     desc = "Go to references" },
+        { "<leader>am", vim.lsp.buf.code_action,    desc = "Code Action" },
     }
+
 }
