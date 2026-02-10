0cd9728a74c7660626ae2a89b7fcbf4a06c4d3e9
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-02-10 19:34:34 +1300

    Squash merge back_to_lazy into main

--- init.lua
+++ init.lua
@@ -1,8 +1,4 @@
 require("config.lazy")
-require("config.lsp")
-require("config.setup")
-require("config.options")
-require("config.keymaps")
 
 -- Theme & UI
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -10,14 +6,3 @@ vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 vim.opt.termguicolors = true -- Enable 24-bit colors
 vim.cmd([[colorscheme tokyodark]])
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end
-})
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,13 +1,50 @@
 return {
-    "neovim/nvim-lspconfig",
-    keys = {
-        { "K",          vim.lsp.buf.hover,          desc = "Hover" },
-        { "<leader>rn", vim.lsp.buf.rename,         desc = "Rename item" },
-        { "<leader>rf", vim.lsp.buf.format,         desc = "Perform Formatting" },
-        { "gd",         vim.lsp.buf.declaration,    desc = "Go to Declaration" },
-        { "gi",         vim.lsp.buf.implementation, desc = "Go to Implementation" },
-        { "gr",         vim.lsp.buf.references,     desc = "Go to references" },
-        { "<leader>am", vim.lsp.buf.code_action,    desc = "Code Action" },
-    }
+  "neovim/nvim-lspconfig",
+  keys = {
+    { "K", vim.lsp.buf.hover, desc = "Hover" },
+    { "<leader>rn", vim.lsp.buf.rename, desc = "Rename item" },
+    { "<leader>rf", vim.lsp.buf.format, desc = "Perform Formatting" },
+    { "gd", vim.lsp.buf.declaration, desc = "Go to Declaration" },
+    { "gi", vim.lsp.buf.implementation, desc = "Go to Implementation" },
+    { "gr", vim.lsp.buf.references, desc = "Go to references" },
+    { "<leader>am", vim.lsp.buf.code_action, desc = "Code Action" },
+  },
+  config = function()
+    local lsp = vim.lsp
 
+    lsp.config("lua_ls", {
+      settins = {
+        diagnostics = { globals = "vim" },
+      },
+    })
+
+    -- paths to check for project.godot file
+    local paths_to_check = { "/", "/../" }
+    local is_godot_project = false
+    local godot_project_path = ""
+    local cwd = vim.fn.getcwd()
+
+    -- iterate over paths and check
+    for key, value in pairs(paths_to_check) do
+      if vim.uv.fs_stat(cwd .. value .. "project.godot") then
+        is_godot_project = true
+        godot_project_path = cwd .. value
+        break
+      end
+    end
+
+    -- check if server is already running in godot project path
+    local is_server_running = vim.uv.fs_stat(godot_project_path .. "/server.pipe")
+    -- start server, if not already running
+    if is_godot_project and not is_server_running then
+      vim.fn.serverstart(godot_project_path .. "/server.pipe")
+    end
+
+    lsp.config("gdscript", {
+      cmd = lsp.rpc.connect("127.0.0.1", tonumber(6005)),
+      filetypes = { "gd", "gdscript", "gdscript3" },
+      root_markers = { "project.godot", ".git" },
+    })
+    lsp.enable("gdscript")
+  end,
 }
